eb3865e3707fbefae683c95393a6c59bfc48fe76
Kuzin <michaelkuzin@gmail.com>
2013-03-27 18:13:57 -0400

    Adding directory structure & dependencies

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'rack'
+gem 'compass', '>=0.13.alpha.0'
--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/images"
-javascripts_dir = "assets/javascripts"
+http_path = "public/"
+css_dir = "public/assets/css"
+sass_dir = "public/assets/scss"
+images_dir = "public/assets/images"
+javascripts_dir = "public/assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
